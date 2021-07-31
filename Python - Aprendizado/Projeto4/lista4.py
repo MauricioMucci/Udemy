3d1926bc1ded7d7b4ff94f32da3978157528be47
MauricioMucci <mauricio_mucci@hotmail.com>
2021-07-31 00:33:15 -0300

    Exercicios sobre Tuplas

--- Python - Aprendizado/Projeto4/lista4.py
+++ Python - Aprendizado/Projeto4/lista4.py
@@ -1,4 +1,4 @@
-#Questao 1
+# Questao 1
 from typing import Counter
 
 
@@ -10,7 +10,9 @@ def QuestaoUm():
     print(a)
     print(f"Soma = {soma}")
 
-#Questao 2
+# Questao 2
+
+
 def QuestaoDois():
     print("\t[ Questao 2 ]")
     array = []
@@ -18,13 +20,15 @@ def QuestaoDois():
         array.append(int(input("\nDigite um inteiro: ")))
     print(array)
 
-#Questao 3
+# Questao 3
+
+
 def QuestaoTres():
     print("\t[ Questao 3 ]")
     array1, array2 = [], []
     for _ in range(10):
         array1.append(float(input("\nDigite um numero: ")))
-    
+
     '''
     array2 = array1.copy()
     for i in range(len(array2)):
@@ -36,13 +40,15 @@ def QuestaoTres():
 
     print(f"{array1}\n{array2}")
 
-#Questao 4
+# Questao 4
+
+
 def QuestaoQuatro():
     print("\t[ Questao 4 ]")
     array = []
     for _ in range(8):
         array.append(int(input("\nDigite um inteiro: ")))
-    
+
     '''
     while True:
         pos1 = int(input("\nDigite a primeira posicao: "))
@@ -53,8 +59,9 @@ def QuestaoQuatro():
         if pos2 >= 0 and pos2 < 8:
             break
     '''
-    
-    pos1, pos2 = int(input("\nDigite a primeira posicao(0-8): ")), int(input("\nDigite a segunda posicao(0-8): "))
+
+    pos1, pos2 = int(input("\nDigite a primeira posicao(0-8): ")
+                     ), int(input("\nDigite a segunda posicao(0-8): "))
     while pos1 < 0 or pos1 > 7:
         pos1 = int(input("\nDigite novamente primeira posicao: "))
     while pos2 < 0 or pos2 > 7:
@@ -62,7 +69,9 @@ def QuestaoQuatro():
     soma = array[pos1] + array[pos2]
     print(f"{array[pos1]} + {array[pos1]} = {soma}")
 
-#Questao 5
+# Questao 5
+
+
 def QuestaoCinco():
     print("\t[ Questao 5 ]")
     array = []
@@ -74,7 +83,9 @@ def QuestaoCinco():
             counter += 1
     print(f"Existe {counter} numeros pares")
 
-#Questao 6
+# Questao 6
+
+
 def QuestaoSeis():
     print("\t[ Questao 6 ]")
     array = []
@@ -82,15 +93,20 @@ def QuestaoSeis():
         array.append(int(input("\nDigite um inteiro: ")))
     print(f"Maior valor = {max(array)}\nMenor valor = {min(array)}")
 
-#Questao 7
+# Questao 7
+
+
 def QuestaoSete():
     print("\t[ Questao 7 ]")
     array = []
     for _ in range(10):
         array.append(int(input("\nDigite um inteiro: ")))
-    print(f"Maior valor = {max(array)}\nPosicao do maior valor = {array.index(max(array))}")
+    print(
+        f"Maior valor = {max(array)}\nPosicao do maior valor = {array.index(max(array))}")
 
-#Questao 8 e 9
+# Questao 8 e 9
+
+
 def QuestaoOitoNove():
     print("\t[ Questao 8 e 9 ]")
     array = []
@@ -104,6 +120,7 @@ def QuestaoOitoNove():
     array.reverse()
     print(array[::-1])
 
+
 def QuestaoDez():
     print("\t[ Questao 10 ]")
     array = []
@@ -113,6 +130,7 @@ def QuestaoDez():
             nota = float(input("\nDigite uma nota valida: "))
         array.append(nota)
     print(f"Media geral = {sum(array)/len(array)}")
+
 
 def QuestaoOnze():
     print("\t[ Questao 11 ]")
@@ -125,7 +143,9 @@ def QuestaoOnze():
             pos_sum += array[i]
         else:
             neg_num += 1
-    print(f"A soma dos positivos = {pos_sum}\nA quantidade de negativos = {neg_num}")
+    print(
+        f"A soma dos positivos = {pos_sum}\nA quantidade de negativos = {neg_num}")
+
 
 def QuestaoDoze():
     print("\t[ Questao 12 ]")
@@ -134,12 +154,15 @@ def QuestaoDoze():
         array.append(int(input("\nDigite um inteiro: ")))
     print(f"{array}\nMaior = {max(array)}\nMedia = {sum(array)/len(array)}")
 
+
 def QuestaoTreze():
     print("\t[ Questao 13 ]")
     array = []
     for i in range(5):
         array.append(int(input("\nDigite um inteiro: ")))
-    print(f"Posicao do Maior = {array.index(max(array))}\nPosicao do Menor = {array.index(min(array))}")
+    print(
+        f"Posicao do Maior = {array.index(max(array))}\nPosicao do Menor = {array.index(min(array))}")
+
 
 def QuestaoQuatorze():
     print("\t[ Questao 14 ]")
@@ -152,7 +175,7 @@ def QuestaoQuatorze():
         rep = array_aux.count(i)
         if rep == 1:
             array_aux.remove(i)
-    
+
     array_aux.sort()
     for i in array_aux:
         rep = array_aux.count(i)
@@ -161,12 +184,13 @@ def QuestaoQuatorze():
 
     print(f"Vetor original: {array}\nNumeros que se repetem: {array_aux}")
 
+
 def QuestaoQuinze():
     print("\t[ Questao 15 ]")
     array, array_aux = [], []
     for i in range(10):
         array.append(int(input("\nDigite um inteiro: ")))
-    
+
     for i in array:
         rep = array.count(i)
         if rep == 1:
@@ -175,7 +199,43 @@ def QuestaoQuinze():
 
     print(f"Vetor original: {array}\nVetor resultante: {array_aux}")
 
-#def QuestaoVinteTres():
+
+def QuestaoVinteTres():
+    print("\t[ Questao 23 ]")
+    conjunto1 = int(input("\nDigite um inteiro: ")), int(input("\nDigite um inteiro: ")), int(input(
+        "\nDigite um inteiro: ")), int(input("\nDigite um inteiro: ")), int(input("\nDigite um inteiro: "))
+    conjunto2 = int(input("\nDigite um inteiro: ")), int(input("\nDigite um inteiro: ")), int(input(
+        "\nDigite um inteiro: ")), int(input("\nDigite um inteiro: ")), int(input("\nDigite um inteiro: "))
+
+    i = 0
+    soma = 0
+    while i < len(conjunto1):
+        soma += conjunto1[i] * conjunto2[i]
+        i += 1
+
+    print(
+        f"Conjunto A = {conjunto1}\nConjunto B = {conjunto2}\nProduto escalar = {soma}")
+
+
+def QuestaoVinteQuatro():
+    print("\t[ Questao 24 ]")
+    array = []
+    for _ in range(5):
+        array.append((int(input("\nDigite o id: ")),
+                     float(input("\nDigite a altura: "))))
+
+    id, altura = [], []
+    for i in array:
+        id.append(i[0])
+        altura.append(i[1])
+
+    for i in range(5):
+        if (id[i], max(altura)) in array:
+            print(
+                f"Aluno #{id[i]} - Altura = {max(altura)}")
+        elif (id[i], min(altura)) in array:
+            print(
+                f"Aluno #{id[i]} - Altura = {min(altura)}")
 
 
 def Main():
@@ -196,7 +256,9 @@ def Main():
     QuestaoQuatorze()
     QuestaoQuinze()
     '''
-    #Tuplas
-    #Questao23()
-    
+    # Tuplas
+    # QuestaoVinteTres()
+    QuestaoVinteQuatro()
+
+
 Main()
